cb09e78a1686fab5badc065ce0bf680718974b7c
Sr-3pp <martin.ru@outlook.com>
2024-03-08 11:48:20 -0600

    Project detail modal

--- pages/projects.vue
+++ pages/projects.vue
@@ -27,31 +27,65 @@ const projectDetails = (project: any) => {
 
 <template lang="pug">
 .projects
-  SrContainer(:with-padding="true")
-    SrText(text="Freelance" tag="h1")
-    SrGrid(tag="ul" class="projects-list")
-      SrGridColumn.projects-list-project(tag="li" :size="{mobile: '1', sm: '1/3'}" v-for="project in freelance" :key="project.name")
-        button(@click="projectDetails(project)")
-          ProjectCard(v-bind="project" :is-vue="true")
-    SrText(text="Contractors" tag="h1")
-    SrGrid(tag="ul" class="projects-list")
-      SrGridColumn.projects-list-project(tag="li" :size="{mobile: '1', sm: '1/3'}" v-for="project in contractors" :key="project.name")
-          SrButton.projects-list-contractor(@click="projectDetails(project)")
-            SrPicture(:src="`/img/logos/${project.logo}`" :alt="project.name" width="200" height="200")
+  section
+    SrPicture.projects-bg(src="/img/stripes.jpg")
+    SrContainer(:with-padding="true")
+      SrText(text="Freelance" tag="h2" class="title")
+      SrGrid(tag="ul" class="projects-list" style="--justify-content: center;")
+        SrGridColumn.projects-list-project(tag="li" :size="{mobile: '1', sm: '1/3'}" v-for="project in freelance" :key="project.name")
+          button(@click="projectDetails(project)")
+            ProjectCard(v-bind="project" :is-vue="true")
+  section
+    SrContainer(:with-padding="true")
+      SrText(text="Contractors" tag="h2" class="title")
+      SrGrid(tag="ul" class="projects-list" style="--justify-content: center;")
+        SrGridColumn.projects-list-project(tag="li" :size="{mobile: '1', sm: '1/4'}" v-for="project in contractors" :key="project.name")
+            SrButton.projects-list-contractor(@click="projectDetails(project)")
+              SrPicture(:src="`/img/logos/${project.logo}`" :alt="project.name" width="200" height="200")
 
-  SrModal(:active="modal_project" @close="modal_project = false")
+  SrModal.projects-project-detail(:active="modal_project" @close="modal_project = false")
     template(#body)
       .sr-modal-body(v-if="currentProject")
-        SrText(:text="currentProject.name")
+        SrGrid
+          SrGridColumn(:size="{mobile: '1', sm: '1/2'}" style="--justify-content: center;")
+            SrPicture.projects-project-detail-thumb(:src="`${currentProject.thumb || `/img/logos/${currentProject.logo}`}`" :alt="currentProject.name" width="200" height="200")
+          SrGridColumn(:size="{mobile: '1', sm: '1/2'}")
+            SrText(:text="currentProject.name" class="title" tag="h2" style="--text-align: center;")
+            SrText(:text="$t(currentProject.description)")
+            SrText(text="Built with" class="subtitle" style="--text-align: center;" tag="h3")
+            SrGrid(tag="ul" style="--justify-content: center;")
+              SrGridColumn(:size="{mobile: '1', sm: '1/5'}" tag="li" v-for="(technology, index) in currentProject.technologies" :key="index")
+                SrPicture(:src="`/img/logos/${technology.logo}`" alt="technology.name" width="100" height="100")
 </template>
 
 <style lang="scss">
 .projects {
-  min-height: calc(100svh - unit(80));
-  background-color: $color-vue-bg;
-  color: $color-vue-text;
-  position: relative;
-  z-index: 1;
+  > section {
+    position: relative;
+    &:nth-child(even) {
+      background: linear-gradient(-135deg, $color-white 80%, $color-vue 90%);
+      color: $color-text;
+    }
+    &:nth-child(odd) {
+      background: $bg-linear-vue-dark;
+      color: $color-vue-text;
+    }
+
+    .sr-container {
+      position: relative;
+    }
+  }
+
+  &-bg.sr-picture {
+    mix-blend-mode: multiply;
+    opacity: 0.2;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+  }
 
   &-list {
     &-project {
@@ -79,6 +113,32 @@ const projectDetails = (project: any) => {
         img {
           border-radius: unit(9999);
           overflow: hidden;
+        }
+      }
+    }
+  }
+
+  &-project {
+    &-detail {
+      color: $color-text;
+
+      .sr-text {
+        margin-bottom: unit(20);
+      }
+
+      .sr-modal-content {
+        max-width: unit(800);
+      }
+
+      &-thumb {
+        padding: unit(20);
+        background: $color-white;
+        border-radius: unit(9999);
+        overflow: hidden;
+        border: {
+          style: solid;
+          width: unit(2);
+          color: $color-vue-hover;
         }
       }
     }
